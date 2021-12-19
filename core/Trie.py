330a0728d78283616bc383e9fb3882a69d901c44
RJ Esquivias <esquiviasrj@gmail.com>
2021-12-19 14:54:52 -0800

    Add Trie

---
+++ core/Trie.py
@@ -0,0 +1,93 @@
+class TrieNode:
+    def __init__(self, terminal=False):
+        self.terminal = terminal
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, s):
+        temp = self.root
+        for c in s:
+            if c not in temp.children:
+                temp.children[c] = TrieNode()
+            temp = temp.children[c]
+
+        temp.terminal = True
+
+    def search(self, s):
+        temp = self.root
+        for c in s:
+            if temp is None or c not in temp.children:
+                return False
+            temp = temp.children[c]
+
+        return temp.terminal if temp is not None else False
+
+    def delete(self, s):
+        self.__delete(s, 0, self.root)
+
+    def __delete(self, s, index, root):
+        if root is None:
+            return root
+
+        if index >= len(s):
+            root.terminal = False
+
+            if self.__has_no_children(root):
+                root = None
+        else:
+            current_char = s[index]
+            root.children[current_char] = self.__delete(s, index + 1, root.children[current_char])
+
+            if self.__has_no_children(root) and root.terminal is False:
+                root = None
+
+        return root
+
+    def __has_no_children(self, root):
+        if root is None: 
+            return False
+
+        return len(root.children) == 0
+
+    def __str__(self):
+        stringList = []
+        self.__buildStringList(self.root, stringList)
+        return str(stringList)
+
+    def __buildStringList(self, root, stringList, currentString = ""):
+        if root is None:
+            return
+
+        if root.terminal is True:
+            stringList.append(currentString)
+
+        for key, value in root.children.items():
+            self.__buildStringList(value, stringList, currentString + key)
+
+
+if __name__ == "__main__":
+    trie = Trie()
+    trie.insert("Hello")
+    trie.insert("World")
+    trie.insert("Another")
+    trie.insert("Way")
+    trie.insert("to")
+    trie.insert("phone")
+    trie.insert("pillow")
+    trie.insert("pills")
+    trie.insert("piller")
+
+    print(trie)
+    print(trie.search("pillow"))
+    trie.delete("pillow")
+    print(trie.search("pills"))
+    print(trie.search("piller"))
+    trie.delete("pills")
+    print(trie.search("pills"))
+    print(trie.search("piller"))
+    trie.delete("piller")
+    print(trie.search("pills"))
+    print(trie.search("piller"))
